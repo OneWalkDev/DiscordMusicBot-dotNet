1939303d3b16f9f5e78e0715bd57b19e2d8bc81b
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-03 16:11:01 +0900

    プロジェクト ファイルを追加します。

---
+++ DiscordMusicBot-dotNet/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DiscordMusicBot_dotNet {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
