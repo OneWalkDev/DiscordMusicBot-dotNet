01dd0cfdc9fa468858fd1e34f1392e90af9e7d05
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-04 07:34:54 +0900

    トークン漏らした

--- DiscordMusicBot-dotNet/TokenManager.cs
+++ DiscordMusicBot-dotNet/TokenManager.cs
@@ -2,7 +2,7 @@
     public class TokenManager {
 
         public TokenManager() {
-            DiscordToken = "ODgyODY1NzQ3OTE3OTM4Njk4.YTBmzQ.nO27Qp1MakqP8n_nHwAaDZFZlHI";
+            DiscordToken = "";
         }
 
         public string DiscordToken { get; private set; }
