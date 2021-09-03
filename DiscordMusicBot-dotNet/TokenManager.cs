5f3c5e8333b1e1858fbe71ea12790b30f3adc053
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-04 07:33:43 +0900

    安定してVCに接続されるように

---
+++ DiscordMusicBot-dotNet/TokenManager.cs
@@ -0,0 +1,11 @@
+﻿namespace DiscordMusicBot_dotNet {
+    public class TokenManager {
+
+        public TokenManager() {
+            DiscordToken = "ODgyODY1NzQ3OTE3OTM4Njk4.YTBmzQ.nO27Qp1MakqP8n_nHwAaDZFZlHI";
+        }
+
+        public string DiscordToken { get; private set; }
+    }
+
+}
